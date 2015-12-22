dd0d7c2ed1255eea17a581564380c77c80aad24f
Oxflick <flick@funkyflick.com>
2015-12-21 17:08:25 -0800

    Added thank you page and added script to process a contact form; added robot picture to the front page plus couple paragraphs of text

---
+++ thanks.php
@@ -0,0 +1,73 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Thank you for contacting me.">
+	<title> Thank you - Oxana Web </title>
+	<link href='https://fonts.googleapis.com/css?family=Raleway:500' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Playfair+Display' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="/css/style.css">
+	<script type="text/javascript" src="/js/jquery-1.11.1.min.js"></script> 
+</head>
+<body>
+	<nav class="navbar navbar-default">
+		<div class="container-fluid">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="/">W</a>
+			</div>
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="/index.html">Home</a></li>
+					<li><a href="/portfolio.html">Portfolio</a></li>
+					<li><a href="/resume.html">Resume</a></li>
+					<li><a href="/contact.html">Contact</a></li>
+				</ul>
+			</div><!-- /.navbar-collapse -->
+		</div><!-- /.container-fluid -->
+	</nav>
+
+	<div class="container">
+		<div class="row">
+			<hr>
+			<div class="panel panel-default">
+				<div class="panel-body">
+					<div class="container-fluid">
+						<div class="col-sm-12 col-md-12 col-lg-8">
+						  <h1> Thank you for contacting me.</h1>
+						  <p class="text-large"> I will be in touch with you shorlty. </p>
+                          <img class="img-responsive" src="/images/robot_web.png" alt="Robot Oxana Web">
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<footer class="footer">
+		<div class="container-fluid">
+			<ul class="footermenu">
+				<li>
+					<a href="https://github.com/oxflick" target="_blank"><img src="/images/github.png" alt="Github"></a>
+				</li>
+				<li>
+					<a href="https://www.linkedin.com/in/oxana-web-b259a380" target="_blank"><img src="/images/linkedin.png" alt="Linkedin"></a>
+				</li>
+			</ul>
+		</div>
+	</footer>
+</body>
+</html>
+
+
+
